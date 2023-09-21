ff6dc14c44bef7dd841b799174e4c94fe77566ae
OmarYasin <73434293+omaradwanyasin@users.noreply.github.com>
2023-09-21 18:24:06 +0300

    TASK 8
    USEEFFECT SOLVED PROPBLEM
    When you pass an object as a dependency, React uses reference equality to determine if the object has changed between renders. If the object reference is different from the previous render, the effect will run. This is because React doesn't perform a deep comparison of object properties.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import "./App.css";
-import React from "react";
-import Input from "./component/Input";
+import React, { useState, useEffect } from "react";
+export default function App() {
+  const [name, setName] = useState(0);
+  const [mobile, setMobile] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+  let dependency = { name, mobile };
 
-const App = () => {
+  // here is the problem react takes an array of dependency and this is an object , so this will be treted as obejct
+  //and change of the object will lead to rerender of all the app  because React doesn't perform a deep comparison of object properties.
+  useEffect(() => {
+    console.log("UseEffect is called");
+  }, [name, mobile]);
   return (
-    <div>
-      <Input />
+    <div className={`container ${darkMode ? "bgDark" : "bgLight"}`}>
+      <input
+        type="text"
+        id="name"
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        id="mobile"
+        placeholder="Mobile"
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <div className="darkmode">
+        <input
+          type="checkbox"
+          id="darkMode"
+          onChange={() => setDarkMode(!darkMode)}
+        />
+        Enable dark mode
+      </div>
     </div>
   );
-};
-export default App;
+}
